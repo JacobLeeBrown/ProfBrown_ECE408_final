87f3f17918aaf7d59dc6256431c4fbd9adc923ed
Jacob Brown <jlbgreenbay@gmail.com>
2018-03-19 20:50:29 -0500

    Finished Milestone2

--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -20,20 +20,32 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     The goal here is to be correct, not fast (this is the CPU implementation.)
     */
 
-    const int B = x.shape_[0];
-    // const int M = y.shape_[1];
-    // const int C = x.shape_[1];
-    // const int H = x.shape_[2];
-    // const int W = x.shape_[3];
-    // const int K = w.shape_[3];
+    const int B = x.shape_[0];     // Batch size
+    const int C = x.shape_[1];     // Input Feature Maps
+    const int H = x.shape_[2];     // Height of input maps
+    const int W = x.shape_[3];     // Width  of input maps
+    const int M = y.shape_[1];     // Output Feature Maps
+    const int H_Out = y.shape_[2]; // Height of output maps
+    const int W_Out = y.shape_[3]; // Width  of output maps
+    const int K = k.shape_[3];     // Filter Dimensions
 
-    for (int b = 0; b < B; ++b) {
+    for (int b = 0; b < B; ++b) { // for each image in the batch 
 
-        CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
+        // CHECK_EQ(0,1) << "Remove this line and replace it with your implementation.";
 
         /* ... a bunch of nested loops later...
             y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
-        */
+        */            
+        for (int m = 0; m < M; ++m)                 // for each output feature maps
+            for (int h = 0; h < H_Out; ++h)         // for each output element
+                for (int w = 0; w < W_Out; ++w)
+                {
+                    y[b][m][h][w] = 0;
+                    for (int c = 0; c < C; ++c)     // sum over all input feature maps
+                        for (int p = 0; p < K; ++p) // KxK filter
+                            for (int q = 0; q < K; ++q)
+                                y[b][m][h][w] += x[b][c][h + p][w + q] * k[m][c][p][q];
+                }
     }
 
 }
