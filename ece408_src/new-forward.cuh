4277bc09925a3c2cc533ea91abb227d86e410717
Brandon Yu <byu23@linux-28.ews.illinois.edu>
2018-03-31 15:08:26 -0500

    Added unroll kernel

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -9,6 +9,33 @@ namespace mxnet
 namespace op
 {
 
+__global__ void unroll(int B, int C, int H, int W, int K, int float *X, float *X_unroll){
+	int c, s, H_out, W_out, h_unroll, w_base, b, p, q;
+	int t = blockId.x * 1024 + threadId.x;
+	int H_out = H-K+1;
+	int W_out = W-K+1;
+	int W_unroll = H_out*W_out;
+
+	if(t<C*W_unroll){
+		c = t/W_unroll;
+		s = t%w_unroll;
+		h_out = s/W_out;
+		w_out = s%W_out;
+		h_unroll = h_out*W_out+w_out;
+		w_base = c*K*K;
+		for(b=0;b<B;b++){
+			for(p=0;p<K;p++){
+				for(q=0;q<K;q++){
+				w_unroll = w_base+p*K+q;
+				X_unroll[b, h_unroll, w_unroll] = X[b, c, h+p, w+q);
+				}
+			}
+		}
+	}
+	
+
+}
+
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
@@ -52,7 +79,23 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
+    const int B = x.shape_[0];     // Batch size
+    const int C = x.shape_[1];     // Input Feature Maps
+    const int H = x.shape_[2];     // Height of input maps
+    const int W = x.shape_[3];     // Width  of input maps
+    const int M = y.shape_[1];     // Output Feature Maps
+    const int H_out = H-K+1; // Height of output maps
+    const int W_out = W-K+1; // Width  of output maps
+    const int K = w.shape_[3];     // Filter Dimensions
     // ...
+
+	// Unroll Kernel Call
+	int num_blocks = ceil(C*H_out*W_out)/1024;
+	int w_unroll = h*W_out+w;
+	int h_unroll = w_base+p*K+q;
+	float *X_unrolled = malloc(B*W_unroll*H_unroll*sizeof(float));
+
+	unroll<<<num_blocks,1024>>>(B, C, H, W, K, x.dptr, X_unrolled); 
 
     // Set the kernel dimensions
     // dim3 gridDim(0);
