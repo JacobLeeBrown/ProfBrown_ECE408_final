82a4a6044155c5978e1bc51e2c153ec6dddb7c2c
Jacob Brown <jlbgreenbay@gmail.com>
2018-04-17 18:41:56 -0500

    Finished milestone 3 code

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -12,6 +12,19 @@ namespace op
 #define BLOCK_SIZE 1024
 #define TILE_WIDTH 16
 
+/*local error check macro*/
+/*
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+*/
+
 // __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 // {
 
@@ -33,8 +46,6 @@ namespace op
 // #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 // #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 // #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
-
 
 // #undef y4d
 // #undef x4d
@@ -78,13 +89,13 @@ __global__ void unroll_Kernel(const int C, const int H_in, const int W_in,
     // Determine which channel the thread maps to
     cur_channel = t / W_unroll;
     // Determine which ouput element the thread maps to, which is equivalent to
-    // the current row we're calculating for the unrolled matrix
+    // the current column we're calculating for the unrolled matrix
     cur_outCol  = t % W_unroll;
 
     // These values are the top left corner of the section we're applying the
     // filter bank to within the input channels 
-    start_inRow = cur_output / W_out; // Starting row in channel maps
-    start_inCol = cur_output % W_out; // Starting column in channel maps
+    start_inRow = cur_outCol / W_out; // Starting row in channel maps
+    start_inCol = cur_outCol % W_out; // Starting column in channel maps
 
     // This value gives us the starting row of the current channel's sections
     // within the unrolled matrix
@@ -92,21 +103,23 @@ __global__ void unroll_Kernel(const int C, const int H_in, const int W_in,
 
     // For each element in the filter bank
     for(p = 0; p < K; p++)
+    {
       for(q = 0; q < K; q++)
       {
         // Now we calculate the exact row index for the unrolled matrix
         cur_outRow = start_outRow + p * K + q;
-        // Finally, we use the macros to unroll the input data
+        // We use the macros to unroll the input data neatly :D
         x2o(cur_outRow, cur_outCol) = x3i(cur_channel, start_inRow + p, start_inCol + q);
       }
+    }
   }
 }
 
 
-__global__ void matrixMultiply(float *A, float *B, float *C, 
-                               int numARows, int numAColumns,
-                               int numBRows, int numBColumns,
-                               int numCRows, int numCColumns)
+__global__ void matrixMultiply(const float *A, const float *B, float *C, 
+                               const int numARows, const int numAColumns,
+                               const int numBRows, const int numBColumns,
+                               const int numCRows, const int numCColumns)
 {
   __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
   __shared__ float subTileB[TILE_WIDTH][TILE_WIDTH];
@@ -176,20 +189,21 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int M = y.shape_[1];      // Output Feature Maps
     const int K = k.shape_[3];      // Filter Dimensions
 
-    const int W_out = W – K + 1;    // Height of output maps
-    const int H_out = H – K + 1;    // Width  of output maps
+    const int W_out = (W - K + 1);    // Height of output maps
+    const int H_out = (H - K + 1);    // Width  of output maps
 
-    const int H_unroll = H_out * W_out; // Height of unrolled matrix
-    const int W_unroll = C * K * K;     // Width  of unrolled matrix
+    const int H_unroll = C * K * K;         // Height of unrolled matrix
+    const int W_unroll = H_out * W_out;     // Width  of unrolled matrix
 
 
     // Allocated the space for the unrolled input (which will get re-written)
-    float* X_unrolled = malloc(W_unroll * H_unroll * sizeof(float));
+    float* x_unrolled;
+    cudaMalloc((void **)&x_unrolled, W_unroll * H_unroll * sizeof(float));
 
 
     // ~~~ Set the kernel dimensions ~~~
     // First we setup for the unroll kernels
-    //const int num_threads = C * H_out * W_out; idky this was in the book, it's unused
+    const int num_threads = C * H_out * W_out;
     const int unroll_blocks = ceil(num_threads / (BLOCK_SIZE * 1.0));
     const dim3 unrollBlocks(BLOCK_SIZE, 1, 1);
     const dim3 unrollGrid(unroll_blocks, 1, 1);
@@ -200,21 +214,21 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const dim3 matrixGrid(ceil(maxCols / (TILE_WIDTH * 1.0)), ceil(maxRows / (TILE_WIDTH * 1.0)), 1);
 
 
+    // Grab the pointer to the filter maps
+    const float* k_ptr = k.dptr_;
+
     // ~~~ Now we begin the bread n butter of all this hard labor ~~~
     // For each image in the batch
     for (int b = 0; b < B; b++)
     {
+      float* x_ptr = &x.dptr_[b*C*H*W];
       unroll_Kernel<<<unrollGrid, unrollBlocks>>>(C, H, W, H_unroll, W_unroll,
-                                                  W_out, K, x[b], X_unrolled);
+                                                  W_out, K, x_ptr, x_unrolled);
       MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-      // Now all we have to do is matrix multiply
-      /* Remember, we treat:
-       * k as a          2D   M by C*K*K matrix,
-       * X_unrolled as a 2D   H_unroll by W_unroll matrix, and
-       * y[b] as a       2D   M by (H_out * W_out = W_unroll) matrix
-       */
-      matrixMultiply<<<matrixGrid, matrixBlocks>>>(k, X_unrolled, y[b],
-                                                   M, C*K*K,
+
+      float* y_ptr = &y.dptr_[b*M*W_out*H_out];
+      matrixMultiply<<<matrixGrid, matrixBlocks>>>(k_ptr, x_unrolled, y_ptr,
+                                                   M, (C*K*K),
                                                    H_unroll, W_unroll,
                                                    M, W_unroll);
       MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
@@ -222,6 +236,9 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     // MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
+
+    // We free the unrolled matrix memory
+    MSHADOW_CUDA_CALL(cudaFree(x_unrolled));
 }
 
 /* 
